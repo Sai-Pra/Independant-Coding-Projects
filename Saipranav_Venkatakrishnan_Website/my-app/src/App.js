c0965aaa3f86124e73bd44567b862930cc33e37d
Saipranav Venkatakrishnan <88897783+Saipranav-Venkatakrishnan@users.noreply.github.com>
2022-11-01 02:22:06 -0500

    Decided on what i wanted for my website
    Set up template for personal profile; I'll work on adding achievements and posts later.

--- Saipranav_Venkatakrishnan_Website/my-app/src/App.js
+++ Saipranav_Venkatakrishnan_Website/my-app/src/App.js
@@ -1,29 +1,52 @@
 import logo from './logo.svg';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import phoenix from './Phoenix.jpg'
 import './App.css';
 
-const theme = createMuiTheme({
-  
-})
+const Header = () => {
+  return (
+    <header style={{
+      backgroundColor: 'blue',
+    }}>
+      <div className = "head-text">
+        <div>
+          <img src={phoenix} align="left" className='Phoenix' />
+          <div align="center" className='Personal-Information'>
+            <h4> Hello World, my name is Sai! </h4>
+            <p style={{
+              fontSize: "50%"
+              }}> 
+              I'm a Computer Engineer at the University of Illinois Urbana Champaign. <br></br>
+              My interests include all things Anime, Personal Coding Projects, and Spicy Food.
+              </p>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    <>
+      <Header />
+    </>
   );
 }
 
