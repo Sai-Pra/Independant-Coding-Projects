d0a8268005655934d037ca9df9c9ac0710a244ff
Saipranav Venkatakrishnan <saipranavvk@gmail.com>
2023-08-12 00:52:10 -0500

    Changed them and styling

--- Website_Attempt_3/app.js
+++ Website_Attempt_3/app.js
@@ -21,6 +21,15 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY === 0) {
+        navbar.style.position = 'fixed';
+    } else {
+        navbar.style.position = 'sticky';
+    }
+});
+
 window.addEventListener("scroll", function() {
     var waves = document.querySelectorAll(".wave");
     var scrollTop = window.scrollY;
